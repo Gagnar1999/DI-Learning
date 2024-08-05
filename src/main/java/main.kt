1f33a56f4af3c0061250909bd202112320c94fc7
Gagan <gagandeep.narang@probusinsurance.com>
2024-08-05 11:24:33 +0530

    Without DI

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -2,7 +2,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.example.dagger.UserRegistrationService
 
 fun main(){
-
+    val userRegService = UserRegistrationService()
+    userRegService.saveUserAndSendEmail("Gagnar1999", "gagannarang1999@gmail.com")
 }
--- src/main/java/org/example/dagger/UserRegistrationService.kt
+++ src/main/java/org/example/dagger/UserRegistrationService.kt
@@ -1,8 +1,21 @@
 package org.example.dagger
 
+/*
+    Issues with this class
+    * Violations of SOLID Principles,
+    * Lifetime of UserService and EmailService Object
+    * Unit Testing (To test User Registration We first have to create userService and EmailService
+    * Extend functionality Issue
+ */
+
 class UserRegistrationService {
 
-    fun saveUserAndSendEmail(){
+    private val userService = UserService()
+    private val emailService = EmailService()
 
+
+    fun saveUserAndSendEmail(userName: String, email: String) {
+        userService.storeUser(userName, email)
+        emailService.sendEmail(userName, email)
     }
 }
