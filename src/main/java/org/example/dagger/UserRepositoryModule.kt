7b64a6c5194fed7a08ba6fd7579aa583cb67016d
Gagan <gagandeep.narang@probusinsurance.com>
2024-08-06 16:27:03 +0530

    Binds annotations when dagger can create object by own then we can use Binds annotations

--- src/main/java/org/example/dagger/UserRepositoryModule.kt
+++ src/main/java/org/example/dagger/UserRepositoryModule.kt
@@ -8,6 +8,11 @@ import dagger.Provides
 @Module
 abstract class UserRepositoryModule {
 
+    /*
+        Binds annotation used here because dagger can create SQLRepository Object by own because it has @Inject Annotations
+        While Using Binds annotations we make it abstract because all methods annotated with Binds are by default abstract
+     */
+
     @Binds
      abstract fun getSQLRepository(sqlRepository: SQLRepository) : UserRepository
 }
